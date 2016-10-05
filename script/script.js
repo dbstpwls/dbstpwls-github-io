7e7369e62140f7383ae451305e79daf13d0c0201
dbstpwls <yunsejin@yunsejin-ui-MacBook-Pro.local>
2016-10-05 19:07:21 +0900

    modify
    수정

---
+++ script/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	init();
+});
+
+function init() {
+	var li_leng = $('.slider li').length,
+		li_name = [1, 3, 5, 7, 9];
+
+	for(var n=0; n<li_leng; n++){
+		$('.menu').append('<li></li>');
+	}
+	$('.slider').css({width : 100*li_leng+'%'});
+	$('.slider>li').css({width : 100/li_leng+'%'});
+}
