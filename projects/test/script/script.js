ee99cf7ae6cfe88f5491f5fa9ed0505e8d8571db
dbstpwls <yunsejin@yunsejin-ui-MacBook-Pro.local>
2016-11-23 15:31:48 +0900

    Day13 update

---
+++ projects/test/script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// var $board = $('.board');
+	// $board.each(function (index) {
+	// 	var text = $(this).children('h2').text();
+	// 	if(text.indexOf('::') == -1){
+	// 		var cc = "bacground";
+	// 		var aa = "red"
+	// 		$(this).children('div').css("background", aa);
+	// 	}
+	// });
+});
